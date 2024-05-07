a6c83dd734b1c1d62f89892bf716a84b3dc1ea78
xuhangc <cxhhg11@gmail.com>
2024-05-07 16:29:26 +0800

    update dataloader

--- data/dataset_RGB.py
+++ data/dataset_RGB.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 import albumentations as A
 import numpy as np
 import torchvision.transforms.functional as F
@@ -39,14 +39,56 @@ class DataLoaderTrain(Dataset):
     def __len__(self):
         return self.sizex
 
+    def mixup(self, inp_img, tar_img, mode='mixup'):
+        mixup_index_ = random.randint(0, self.sizex - 1)
+
+        mixup_inp_path = self.inp_filenames[mixup_index_]
+        mixup_tar_path = self.tar_filenames[mixup_index_]
+
+        mixup_inp_img = Image.open(mixup_inp_path).convert('RGB')
+        mixup_tar_img = Image.open(mixup_tar_path).convert('RGB')
+
+        mixup_inp_img = np.array(mixup_inp_img)
+        mixup_tar_img = np.array(mixup_tar_img)
+
+        transformed = self.transform(image=mixup_inp_img, target=mixup_tar_img)
+
+        alpha = 0.2
+        lam = np.random.beta(alpha, alpha)
+
+        mixup_inp_img = F.to_tensor(transformed['image'])
+        mixup_tar_img = F.to_tensor(transformed['target'])
+
+        if mode == 'mixup':
+            inp_img = lam * inp_img + (1 - lam) * mixup_inp_img
+            tar_img = lam * tar_img + (1 - lam) * mixup_tar_img
+        elif mode == 'cutmix':
+            img_h, img_w = self.img_options['h'], self.img_options['w']
+
+            cx = np.random.uniform(0, img_w)
+            cy = np.random.uniform(0, img_h)
+
+            w = img_w * np.sqrt(1 - lam)
+            h = img_h * np.sqrt(1 - lam)
+
+            x0 = int(np.round(max(cx - w / 2, 0)))
+            x1 = int(np.round(min(cx + w / 2, img_w)))
+            y0 = int(np.round(max(cy - h / 2, 0)))
+            y1 = int(np.round(min(cy + h / 2, img_h)))
+
+            inp_img[:, y0:y1, x0:x1] = mixup_inp_img[:, y0:y1, x0:x1]
+            tar_img[:, y0:y1, x0:x1] = mixup_tar_img[:, y0:y1, x0:x1]
+
+        return inp_img, tar_img
+    
     def __getitem__(self, index):
         index_ = index % self.sizex
 
         inp_path = self.inp_filenames[index_]
         tar_path = self.tar_filenames[index_]
 
-        inp_img = Image.open(inp_path).convert('L')
-        tar_img = Image.open(tar_path).convert('L')
+        inp_img = Image.open(inp_path).convert('RGB')
+        tar_img = Image.open(tar_path).convert('RGB')
 
         inp_img = np.array(inp_img)
         tar_img = np.array(tar_img)
@@ -56,7 +98,13 @@ class DataLoaderTrain(Dataset):
         inp_img = F.to_tensor(transformed['image'])
         tar_img = F.to_tensor(transformed['target'])
 
-        filename = os.path.splitext(os.path.split(tar_path)[-1])[0]
+        if index_ > 0 and index_ % 3 == 0:
+            if random.random() > 0.5:
+                inp_img, tar_img = self.mixup(inp_img, tar_img, mode='mixup')
+            else:
+                inp_img, tar_img = self.mixup(inp_img, tar_img, mode='cutmix')
+
+        filename = os.path.basename(tar_path)
 
         return inp_img, tar_img, filename
 
@@ -90,8 +138,8 @@ class DataLoaderVal(Dataset):
         inp_path = self.inp_filenames[index_]
         tar_path = self.tar_filenames[index_]
 
-        inp_img = Image.open(inp_path).convert('L')
-        tar_img = Image.open(tar_path).convert('L')
+        inp_img = Image.open(inp_path).convert('RGB')
+        tar_img = Image.open(tar_path).convert('RGB')
 
         if not self.img_options['ori']:
             inp_img = np.array(inp_img)
@@ -105,6 +153,6 @@ class DataLoaderVal(Dataset):
         inp_img = F.to_tensor(inp_img)
         tar_img = F.to_tensor(tar_img)
 
-        filename = os.path.splitext(os.path.split(tar_path)[-1])[0]
+        filename = os.path.basename(tar_path)
 
         return inp_img, tar_img, filename
